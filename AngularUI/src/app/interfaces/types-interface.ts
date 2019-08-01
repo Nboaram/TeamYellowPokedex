4fc14a8f6c8e09564e720b7cf3751be44828a5ab
Daniel Little <daniel.r.little.1994@gmail.com>
2019-08-01 10:16:51 +0100

    currently adding types to pokemon

---
+++ AngularUI/src/app/interfaces/types-interface.ts
@@ -0,0 +1,7 @@
+import { TypeInterface } from 'src/app/interfaces/type-interface';
+import { NumberInterface } from './number-interface';
+
+export interface TypesInterface {
+    0: NumberInterface
+    1: NumberInterface
+}
